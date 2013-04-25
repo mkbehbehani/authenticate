52caccd13302fc63c651259c3c954a4b12247d5d
Mashallah Behbehani <mkbehbehani@gmail.com>
2013-04-25 18:58:34 -0500

    Adds method for session creation

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,2 +1,14 @@
 class SessionController < ApplicationController
+	def create
+		user = User.find_by_email(params[:email])
+	if user && user.authenticate
+		session[:user_id] = user.user_id
+		redirect_to root_url
+	else
+		render 'new'
+	end
+
+	def destroy
+
+	end
 end
