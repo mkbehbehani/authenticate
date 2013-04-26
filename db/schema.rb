2c723cabe06c1b418ebb2f3207ec7fc75a3138a7
Mashallah Behbehani <mkbehbehani@gmail.com>
2013-04-25 22:11:26 -0500

    Adds security question and answer to view, migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130425233001) do
+ActiveRecord::Schema.define(:version => 20130426030053) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(:version => 20130425233001) do
     t.string   "password_digest"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.string   "auth_token"
+    t.text     "question"
   end
 
 end
