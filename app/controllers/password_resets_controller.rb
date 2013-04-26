c7f8abc3dad44b247ad8620a688d28fd48eef3e0
Mashallah Behbehani <mkbehbehani@gmail.com>
2013-04-25 23:05:13 -0500

    Creates password reset method

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,11 @@
 class PasswordResetsController < ApplicationController
   def new
   end
+
+  def create
+  	u = User.find_by_email(params[:email])
+  	u.send if u
+  	redirect_to root_url
+  	
+  end
 end
