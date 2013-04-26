d605fef7732bce6c1c29678fca35f6206b59c3cb
Mashallah Behbehani <mkbehbehani@gmail.com>
2013-04-26 00:59:52 -0500

    Modifies views, adds mailer log to readme

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
   attr_accessible :email, :name, :password, :password_confirmation, :auth_token, :question
-  validates_presence_of :email, :name, :password, :password_confirmation, :auth_token, :question, :on => :create
+  validates_presence_of :email, :name, :password, :password_confirmation, :on => :create
   
 
   before_create { generate_token (:auth_token)}
