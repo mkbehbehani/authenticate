683e945bc127899beb3a5168cf7d35ba6da8dfa3
Mashallah Behbehani <mkbehbehani@gmail.com>
2013-04-26 00:39:38 -0500

    Fixes errors in edit view

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -14,7 +14,16 @@ class PasswordResetsController < ApplicationController
   end
 
   def edit
-  	@user = User.find.by.password_reset_token!(params[:id])
+  	@user = User.find_by_password_reset_token!(params[:id])
   	#routes to the correct edit page, using the token passed into the url from the email
+  end
+
+  def update
+  	#standard rails CRUD method
+  	@user = User.find_by_password_reset_token!(params[:id])
+  	if @user.update_attributes(params[:user])
+
+  	redirect_to root_url, :notice => "Password Updated!"
+  	end
   end
 end
