59844a630ef4b0f46ee5a2c6c311c52f66886ebe
Mashallah Behbehani <mkbehbehani@gmail.com>
2013-04-25 21:21:53 -0500

    Modifies session controller authentication params

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
 	# checking whether the authenticate method passes back as "true"
 	def create
 	user = User.find_by_email(params[:email])
-		if user && user.authenticate
+		if user && user.authenticate(params[:password])
 			session[:user_id] = user.id
 			redirect_to root_url
 		else
