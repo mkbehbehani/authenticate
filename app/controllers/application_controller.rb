158bc4dc363424449e2496c6e4f3f1344f5f0a23
Mashallah Behbehani <mkbehbehani@gmail.com>
2013-04-25 21:32:03 -0500

    Adds application methods for current_user and authenticate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  private
+  def current_user
   # @current_user will only be valid if there is a session id other than nil
-  # @current_user = User.find(session[:user_id])
-
+  	@current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
   # helper_method allows instance variable @current_user to be accessed from the view
   helper_method :current_user
-  
+  def authorize
+    redirect_to login_url if current_user.nil?
+  end
 end
