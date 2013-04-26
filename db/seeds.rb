d69a7bf5ca4318e5b82e75e9737bec796051fc1a
Mashallah Behbehani <mkbehbehani@gmail.com>
2013-04-26 13:09:42 -0500

    Adds documentation

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 User.create(name: 'Albert Einstein', email:'einstein@gmail.com', password:'apple', password_confirmation:'apple')
-User.create(name: 'Nikolai Tesla', email:'tesla@gmail.com', password:'banana', password_confirmation:'banana')
+User.create(name: 'Nikola Tesla', email:'tesla@gmail.com', password:'banana', password_confirmation:'banana')
 User.create(name: 'Alessandro Volta', email:'volta@gmail.com', password:'tangelo', password_confirmation:'tangelo')
 User.create(name: 'Edwin Hubble', email:'hubble@gmail.com', password:'pineapple', password_confirmation:'pineapple')
 User.create(name: 'Galileo Galilei', email:'galileo@gmail.com', password:'mango', password_confirmation:'mango')
