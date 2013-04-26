9be40f87bb1e028b5513e6600f5c078ba3be7e81
Mashallah Behbehani <mkbehbehani@gmail.com>
2013-04-26 00:24:06 -0500

    Creates password edit page

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -12,4 +12,9 @@ class PasswordResetsController < ApplicationController
   	redirect_to root_url
 
   end
+
+  def edit
+  	@user = User.find.by.password_reset_token!(params[:id])
+  	#routes to the correct edit page, using the token passed into the url from the email
+  end
 end
