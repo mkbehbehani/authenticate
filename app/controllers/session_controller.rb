ae294da79af724c0a15c17d0a17c77eb49cf7959
Mashallah Behbehani <mkbehbehani@gmail.com>
2013-04-25 19:30:38 -0500

    Adds current_user instance variable to application controller, session bugfix

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -4,7 +4,7 @@ class SessionController < ApplicationController
 	def create
 		user = User.find_by_email(params[:email])
 	if user && user.authenticate
-		session[:user_id] = user.user_id
+		session[:user_id] = user.id
 		redirect_to root_url
 	else
 		render 'new'
